492ab58f4a95e9169b3c3138c49f14921385cd33
antcha19 <antcha@alu.iesserpis.org>
2019-10-03 09:01:02 +0200

    Añadido JHolaMundo(cambio introducir por teclado)

--- JHolaMundo/src/iesserpis/ed/HolaMundo.java
+++ JHolaMundo/src/iesserpis/ed/HolaMundo.java
@@ -1,9 +1,22 @@
 package iesserpis.ed;
 
+import java.util.Scanner;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo desde eclipse");
+		
+		Scanner scanner = new Scanner(System.in); //varible para introducir por taclado
+		
+		System.out.println("introduce  tu nombre"); // imprimir por pantalla
+		String nombre = scanner.nextLine();
+		
+		System.out.println("introduce  tu edad?");
+		int edad = scanner.nextInt();
+		
+		//imprimi y introduce por teclado
+		System.out.println("mi nombre es " + nombre + " y tengo " + edad + " años");
 	}
 
 }
+ 

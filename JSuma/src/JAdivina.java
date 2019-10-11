8e2e9172f8252de23c845f2baba118d9aa478aa0
antcha19 <antcha@alu.iesserpis.org>
2019-10-11 08:04:13 +0200

    inslatacion java en mv

---
+++ JSuma/src/JAdivina.java
@@ -0,0 +1,31 @@
+/*
+ * Antonio Yepez
+ * 07102019
+ * programa  para adivinar un numero
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class JAdivina {
+
+	public static void main(String[] args) {
+		
+		//variable para introducir por pantalla
+		Scanner sc=new Scanner(System.in);
+		int	x;
+		
+
+		System.out.println("numero");
+		x  = sc.nextInt();
+	
+		while (x !=0) {
+			System.out.println("introduce un numeor (0 para terminar):");	
+			x = sc.nextInt();
+			
+		}
+			System.out.println("fin del programa");
+	}
+	
+	
+
+}

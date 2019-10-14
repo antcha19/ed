4e64341da9a5d4ff58db8eab6c41db55c4823189
antcha19 <antcha@alu.iesserpis.org>
2019-10-14 13:13:46 +0200

    actualizado

--- JSuma/src/JAdivina.java
+++ JSuma/src/JAdivina.java
@@ -3,29 +3,27 @@
  * 07102019
  * programa  para adivinar un numero
  */
-import java.util.Random;
+
 import java.util.Scanner;
 
 public class JAdivina {
 
 	public static void main(String[] args) {
 		
-		//variable para introducir por pantalla
-		Scanner sc=new Scanner(System.in);
-		int	x;
+		int	contador=1;
 		
-
-		System.out.println("numero");
-		x  = sc.nextInt();
 	
-		while (x !=0) {
-			System.out.println("introduce un numero (0 para terminar):");	
-			x = sc.nextInt();
-			
+		while (contador <=5) {
+		System.out.println("contador = " + contador);	
+		contador= contador + 1;
 		}
-			System.out.println("fin del programa");
-	}
+		System.out.println("finde del programa");
+			
 	
+		//for
+//		for(int contador = 1; contador <=5; contador++)
+//			System.out.println("contador = " + contador);
+//			System.out.println("fin del programa");
+	}			
 	
-
 }

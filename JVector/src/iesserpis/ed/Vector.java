c7589177822bef1b397eb9190d51ed7b52db54a5
antcha19 <antcha@alu.iesserpis.org>
2019-10-14 13:59:59 +0200

    vector

---
+++ JVector/src/iesserpis/ed/Vector.java
@@ -0,0 +1,18 @@
+package iesserpis.ed;
+
+public class Vector {
+
+	public static void main(String[] args) {
+		
+		//int index=0;
+		//vector
+		int [] v= {17,12,15,9,14};
+		
+		for (int index=0; index<5; index++);
+		 System.out.println("elemento " + index +" valor="+  v[ index ]);
+		
+		//System.out.println("v[0] = " + v[ index ]);// utliza la variable index
+	//	System.out.println("v[1] = " + v[1]);
+	}
+
+}

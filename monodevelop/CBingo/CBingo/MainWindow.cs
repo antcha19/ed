5550c4ff11d10908bee5c72015f3f148f67c3797
antcha19 <antcha@alu.iesserpis.org>
2020-02-06 08:18:54 +0100

    cbingo

---
+++ monodevelop/CBingo/CBingo/MainWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Gtk;
+using CBingo;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        Panel panel = new Panel(vbox1);
+       /* Bombo bombo = new Bombo();
+        BJugar.Clicked +=delegate
+        {
+            int numero = bombo.sacarbola();
+
+        };*/
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+   protected void OnBJugarClicked(object sender, EventArgs e)
+    {
+
+        int numero = Bombo.sacarbola();
+
+    }
+}
+

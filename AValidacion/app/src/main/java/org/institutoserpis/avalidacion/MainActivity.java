b44e9a80faabb63068fcf60322336ca05b88606e
antcha19 <55527269+antcha19@users.noreply.github.com>
2020-04-05 23:43:37 +0200

    validacion

---
+++ AValidacion/app/src/main/java/org/institutoserpis/avalidacion/MainActivity.java
@@ -0,0 +1,46 @@
+package org.institutoserpis.avalidacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText et1,et2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.text_nombre);
+        et2 = (EditText) findViewById(R.id.text_contrasena);
+    }
+
+
+    public void validar (View view){
+
+        String nombre = et1.getText().toString();
+        String contrasena = et1.getText().toString();
+
+        if (nombre.length() ==0){
+            Toast.makeText(this,"DEBES INTRODUCIR UN NOMBRE", Toast.LENGTH_LONG).show();
+        }
+        if (contrasena.length() ==0){
+            Toast.makeText(this,"DEBES INTRODUCIR UNA CONTRASEÑA", Toast.LENGTH_LONG).show();
+        }
+        if (nombre.length() !=0 &&  contrasena.length() !=0){
+            Toast.makeText(this,"VALIDANDO", Toast.LENGTH_LONG).show();
+            Intent validar = new Intent(this, SegundaActivity.class);
+            validar.putExtra("nombre",et1.getText().toString());
+            startActivity(validar);
+
+        }
+    }
+}
---
+++ AValidacion/app/src/main/java/org/institutoserpis/avalidacion/SegundaActivity.java
@@ -0,0 +1,30 @@
+package org.institutoserpis.avalidacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView tv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        tv2= (TextView) findViewById(R.id.textparte);
+        String info=getIntent().getStringExtra("nombre");
+        tv2.setText("Bienvenido " + info);
+
+    }
+    public void cerrar(View view){
+
+        Intent cerrar = new Intent(this, MainActivity.class);
+        startActivity(cerrar);
+    }
+}
